ca25dca25edc0415911add7ddc5af8dccd8a3eab
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2020-03-31 20:48:53 +0700

    change Role to IsAdministrator

--- models/user.go
+++ models/user.go
@@ -6,16 +6,11 @@ import (
 
 type Role string
 
-const (
-	Administrator Role = "Administrator"
-	AuthorizedUser Role = "AuthorizedUser"
-)
-
 type User struct {
-	UserID    string    `json:"user_id"`
-	Username  string    `json:"username"`
-	Salt      string    `json:"salt"`
-	Hash      string    `json:"hash"`
-	Role      Role      `json:"role"`
-	CreatedAt time.Time `json:"created_at"`
+	UserID          string    `json:"user_id"`
+	Username        string    `json:"username"`
+	Salt            string    `json:"salt"`
+	Hash            string    `json:"hash"`
+	IsAdministrator bool      `json:"is_administrator"`
+	CreatedAt       time.Time `json:"created_at"`
 }
