7bae427ffe9c92942e694404a76de246ee70dc7d
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2020-04-04 21:29:56 +0700

    wip

--- src/permission/repository.go
+++ src/permission/repository.go
@@ -3,6 +3,6 @@ package permission
 import "github.com/KitaPDev/fogfarms-server/models"
 
 type Repository interface {
-	AssignUserToModuleGroup(username string, moduleGroupID string, level models.Level)
+	AssignUserToModuleGroup(username string, moduleGroupID string, permissionLevel int)
 	GetSupervisorModuleGroups(userID string) []models.ModuleGroup
 }
--- src/permission/services.go
+++ src/permission/services.go
@@ -2,18 +2,12 @@ package permission
 
 import (
 	"github.com/KitaPDev/fogfarms-server/models"
-	"github.com/KitaPDev/fogfarms-server/src/permission/repository"
 )
 
-func AssignUserToModuleGroup(username string, moduleGroupID string, level string) {
-	var l models.Level
-	if level == "Supervisor" || level == "Control" || level == "Monitor" {
-		l = models.Level(level)
-	}
-
-	repository.AssignUserToModuleGroup(username, moduleGroupID, l)
+func AssignUserToModuleGroup(username string, moduleGroupID string, permissionLevel int) {
+	//repository.AssignUserToModuleGroup(username, moduleGroupID, permissionLevel)
 }
 
 func GetSupervisorModuleGroups(user *models.User) []models.ModuleGroup {
-
+	return nil
 }
