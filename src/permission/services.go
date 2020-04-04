8a9985b777386d43282faaf518bec8460d88a315
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2020-04-04 21:30:44 +0700

    wip

--- src/permission/services.go
+++ src/permission/services.go
@@ -2,10 +2,11 @@ package permission
 
 import (
 	"github.com/KitaPDev/fogfarms-server/models"
+	"github.com/KitaPDev/fogfarms-server/src/permission/repository"
 )
 
 func AssignUserToModuleGroup(username string, moduleGroupID string, permissionLevel int) {
-	//repository.AssignUserToModuleGroup(username, moduleGroupID, permissionLevel)
+	repository.AssignUserToModuleGroup(username, moduleGroupID, permissionLevel)
 }
 
 func GetSupervisorModuleGroups(user *models.User) []models.ModuleGroup {
