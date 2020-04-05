00ebd882872a558c210ee14ad4d555df0d15bc23
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2020-04-05 23:41:57 +0700

    add folder /src/pkg/ for go.mod support on GoLand

--- src/user/repository.go
+++ src/user/repository.go
@@ -5,4 +5,5 @@ import "github.com/KitaPDev/fogfarms-server/models"
 type Repository interface {
 	GetAllUsers() []models.User
 	CreateUser(username string, password string, isAdministrator bool)
+	ValidateUser(username string, inputPassword string) bool
 }
