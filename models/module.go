75b5ea5bfd9eaae099fd879dab0526b637dd3a4f
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2020-04-12 16:59:32 +0700

    update dashboard

--- models/module.go
+++ models/module.go
@@ -4,4 +4,5 @@ type Module struct {
 	ModuleID      int    `json:"module_id"`
 	ModuleGroupID int    `json:"module_group_id"`
 	Token         string `json:"token"`
+	ModuleLabel   string `json:"module_label"`
 }
