3d2c03e15c6a61412f0d448443128a832e911b9a
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2020-04-14 02:15:04 +0700

    update schema

--- data/schema.sql
+++ data/schema.sql
@@ -34,24 +34,25 @@ CREATE TABLE Users (
 
 -- Nutrient
 CREATE TABLE Nutrient (
-    NutrientID SERIAL NOT NULL PRIMARY KEY,
+    NutrientID SERIAL NOT NULL,
     Part INT NOT NULL,
     Nitrogen INT NOT NULL,
     Phosphorus INT NOT NULL,
     Potassium INT NOT NULL,
+    PRIMARY KEY (NutrientID),
     UNIQUE (Part, Nitrogen, Phosphorus, Potassium)
 );
 
 -- PHUpUnit
 CREATE TABLE PHUpUnit (
-    PHUpUnitID SERIAL NOT NULL PRIMARY KEY,
+    PHUpUnitID SERIAL NOT NULL,
     NutrientUnitID INT NOT NULL,
     PRIMARY KEY (PHUpUnitID),
     FOREIGN KEY (NutrientUnitID) REFERENCES NutrientUnit (NutrientUnitID)
 );
 
 CREATE TABLE PHDownUnit (
-    PHDownUnitID SERIAL NOT NULL PRIMARY KEY,
+    PHDownUnitID SERIAL NOT NULL,
     NutrientUnitID INT NOT NULL,
     PRIMARY KEY (PHDownUnitID),
     FOREIGN KEY (NutrientUnitID) REFERENCES NutrientUnit (NutrientUnitID)
