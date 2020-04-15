603ecee368d4681cd3fb15abab8cd0a898b8eacc
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2020-04-16 00:22:24 +0700

    add db credentials for fogfarms-server herokuapp

--- src/database/database.go
+++ src/database/database.go
@@ -17,11 +17,12 @@ var db *sql.DB
 // 	DbName = "fogfarms-01"
 // )
 const (
-	DbHost = "localhost"
+	DbHost = "ec2-54-157-78-113.compute-1.amazonaws.com"
 	DbPort = 5432
-	DbUser = "postgres"
-	DbPass = "postgres"
-	DbName = "fogfarms-01"
+	DbUser = "aevojjwxgydmym"
+	DbPass = "f3940dee95e22cb02e4dea372a8252fa356265e2b3fde6875307ed563e88f639"
+	DbName = "dbfi6i4j26c9jj"
+	SSLMODE = "require"
 )
 
 func GetDB() *sql.DB {
@@ -29,8 +30,8 @@ func GetDB() *sql.DB {
 
 	if db == nil {
 		connectionString := fmt.Sprintf("port=%d host=%s user=%s "+
-			"password=%s dbname=%s sslmode=disable",
-			DbPort, DbHost, DbUser, DbPass, DbName)
+			"password=%s dbname=%s sslmode=%s",
+			DbPort, DbHost, DbUser, DbPass, DbName, SSLMODE)
 
 		db, err = sql.Open("postgres", connectionString)
 		if err != nil {
